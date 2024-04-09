148171bca8b6565e861ad50d53b7789c55aadc82
NateSitthi <143848445+NateSitthi@users.noreply.github.com>
2024-04-09 15:47:22 -0700

    JavaScript needs fixing.

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-document.
+var playerCount;
+
+function onePlayer() {
+    document.getElementById("1Player")
+    playerCount = 1;
+    console.log(playerCount)
+}
+function twoPlayer() {
+    document.getElementById("2Player")
+    playerCount = 2;
+    console.log(playerCount)
+}
