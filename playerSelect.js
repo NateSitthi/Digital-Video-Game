d7e44a9757471aa121c435b5fc598e9610b03ed5
NateSitthi <143848445+NateSitthi@users.noreply.github.com>
2024-04-11 15:47:10 -0700

    4/11

--- playerSelect.js
+++ playerSelect.js
@@ -1,18 +1,32 @@
 var playerCount = 0;
 let roundsTotal = 0;
 let roundsPlayed = 0;
+let colors = ["Red", "Blue", "Cyan", "Pink", "Brown", "Green", "Black", "Gold"];
+let combination = [];
+
+
 
 function onePlayer() {
-    document.getElementById("1Player")
+    document.getElementById("1Player");
     playerCount = 1;
-    console.log(playerCount)
-    let slider = document.getElementById("myRange")
+    console.log(playerCount);
+    let slider = document.getElementById("myRange");
     slider = slider.style.display = "none";
 }
 function twoPlayer() {
-    document.getElementById("2Player")
+    document.getElementById("2Player");
     playerCount = 2;
-    console.log(playerCount)
-    let slider = document.getElementById("myRange")
-    slider = slider.style.display = "block";
+    console.log(playerCount);
+    let slider = document.getElementById("myRange");
+    slider = slider.style.display = "block"; // shows slider
+}
+function ready() {
+    console.log(combination.length);
+    
+    if (playerCount == 1) {
+        while (combination.length < 4) {
+            // use splice tag and someway to randomize combination
+        }
+    }
+
 }
