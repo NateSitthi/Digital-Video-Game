47890dfa09ed3838cc9ec9ecd6d1ba06bdc8566a
NateSitthi <143848445+NateSitthi@users.noreply.github.com>
2024-04-19 01:55:36 -0700

    4/18

--- playerSelect.js
+++ playerSelect.js
@@ -1,12 +1,12 @@
 
-let playerCount = 0;
-let roundsTotal = 0;
-let roundsPlayed = 0;
-let colors = ['Red', 'Blue', 'Cyan', 'Pink', 'Brown', 'Green', 'Black', 'Gold'];
+let colors = [
+ 'Red', 'Blue', 'Cyan','Pink', 'Brown', 'Green', 'Black', 'Gold'
+]
 let combination = [];
 let userComb = [];
 let randComb = colors[Math.floor(Math.random() * colors.length)];
 let guess = 8;
+let win = false
 
 
 function ready() {
@@ -19,13 +19,31 @@ function ready() {
         }
 
 }
-
-let Red = "Red"
-let Blue = "Blue"
 function updateUserlist(color) {
     userComb.push(color);
     console.log(userComb);
-    if (userComb.length < 4) {
+}
+function game(){
+    ready();
+    console.log("User Combination:", userComb);
+    console.log("CPU Combination:", combination);
+}
+function compare () {
+    let codeCopy = randComb.slice(0);
+if (randComb === userComb) {
+    for (let i = 0; i < array.length; i++) {
+        if (randComb[i] === userComb[i]) {
+            win = true;
+            console.log(win)
+        }
         
-    };
+    }
+}
+else {
+    for (let j = 0; j < randComb.length; j++) {
+        if (codeCopy.indexOf(guess[j]) !== -1) {
+          codeCopy[codeCopy.indexOf(guess[j])] = 0;
+        }
+      }
+}
 }
