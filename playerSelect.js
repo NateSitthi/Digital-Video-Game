ee5e7ce3b73f865a6e21d71ed0ff1b3992603f2f
NateSitthi <143848445+NateSitthi@users.noreply.github.com>
2024-04-19 15:49:13 -0700

    4/19

--- playerSelect.js
+++ playerSelect.js
@@ -1,49 +1,51 @@
-
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('loaded')
+})
 let colors = [
- 'Red', 'Blue', 'Cyan','Pink', 'Brown', 'Green', 'Black', 'Gold'
+    'Red', 'Blue', 'Cyan', 'Pink', 'Brown', 'Green', 'Black', 'Gold'
 ]
 let combination = [];
 let userComb = [];
+let S1 = document.getElementById("S1"),S2 = document.getElementById("S2"),S3 = document.getElementById("S3"),S4 = document.getElementById("S4")
+let userGuess = 8;
 let randComb = colors[Math.floor(Math.random() * colors.length)];
 let guess = 8;
 let win = false
-
+let list = document.getElementById
 
 function ready() {
     conButton = document.getElementById('confirm');
-    conButton.style.display ='none';
-        while (combination.length < 4) {
-            let randComb = colors[Math.floor(Math.random() * colors.length)];
-            combination.push(randComb);
-            console.log(combination);
-        }
+    // conButton.style.display ='none';
+    while (combination.length < 4) {
+        let randComb = colors[Math.floor(Math.random() * colors.length)];
+        combination.push(randComb);
+        console.log(combination);
+    }
 
 }
 function updateUserlist(color) {
     userComb.push(color);
     console.log(userComb);
+    console.log(color);
+    document.getElementById("S1").classList=" "
+    document.getElementById("S1").classList.add("dot",color)
 }
-function game(){
+function game() {
     ready();
     console.log("User Combination:", userComb);
     console.log("CPU Combination:", combination);
-}
-function compare () {
-    let codeCopy = randComb.slice(0);
-if (randComb === userComb) {
-    for (let i = 0; i < array.length; i++) {
-        if (randComb[i] === userComb[i]) {
-            win = true;
-            console.log(win)
+    if (userComb === guess) {
+        console.log("testing1")
+        if (randComb === userComb) {
+            // for (let i = 0; i < array.length; i++) {
+            //     if (randComb[i] === userComb[i]) {
+            //         win = true;
+            //         console.log("this works")
+            //     }
+            // }
         }
-        
     }
 }
-else {
-    for (let j = 0; j < randComb.length; j++) {
-        if (codeCopy.indexOf(guess[j]) !== -1) {
-          codeCopy[codeCopy.indexOf(guess[j])] = 0;
-        }
-      }
-}
-}
+
+
+
